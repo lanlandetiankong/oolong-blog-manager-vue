555c2e1c80cc4136d60c1d81885f1ece3d7f04c5
lanlandetiankong <695605813@qq.com>
2021-01-12 16:30:43 +0800

    OblArticleDisplayView 动态传参

--- src/pages/index/obl/article/display/OblArticleDisplayView.vue
+++ src/pages/index/obl/article/display/OblArticleDisplayView.vue
@@ -1,11 +1,48 @@
 <template>
-    <h1>OblArticleDisplayView</h1>
+    <h1>OblArticleDisplayView {{formFid}}</h1>
 </template>
 
 <script>
-export default {
-    name: "OblArticleDisplayView"
-}
+    import {OblArticleDisplayApi} from './oblArticleDisplayApi'
+    export default {
+        name: "OblArticleDisplayView",
+        data(){
+            return {
+                formConf:{}
+            }
+        },
+        computed:{
+            formFid(){
+                if(this.queryConf){
+                    return this.queryConf.fid ;
+                }
+                return '';
+            },
+            queryConf(){
+                return this.$route.query ;
+            }
+        },
+        watch: {
+            formFid(oval,nval){
+                if(!nval){
+                    return ;
+                }
+                this.renderObj();
+            }
+        },
+        methods:{
+            renderObj(){
+                if(!this.formFid){
+                    return ;
+                }
+                OblArticleDisplayApi.getItemById(this.formFid).then((res) =>{
+                    if(res.success){
+                        _this.formConf = res.bean ;
+                    }
+                });
+            }
+        }
+    }
 </script>
 
 <style scoped>
---
+++ src/pages/index/obl/article/display/oblArticleDisplayApi.js
@@ -0,0 +1,16 @@
+//ajax远程调用
+import axios from '~Config/axios/httpConfig'
+//包装param参数
+import qs from 'qs'
+
+/* 不要使用 // 进行注释！！！！！！！！！！！！！！！！！！！！！！！！   */
+
+export const OblArticleDisplayApi = {
+    getItemById(fid){  //根据 文章id 取得文章信息
+        var params = {
+            fid:fid
+        }
+        return axios.post("/oblCtl/oblArticle/queryOneById",qs.stringify(params)).then(res => res.data);
+    }
+
+}
