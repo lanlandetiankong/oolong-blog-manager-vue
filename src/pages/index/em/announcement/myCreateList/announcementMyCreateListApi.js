5e3666e9fe08ed4c143263b0c639c56ebab8ca08
lanlandetiankong <695605813@qq.com>
2021-02-07 16:14:14 +0800

    接口变更- /gainEnumSelect 授权相关

--- src/pages/index/em/announcement/myCreateList/announcementMyCreateListApi.js
+++ src/pages/index/em/announcement/myCreateList/announcementMyCreateListApi.js
@@ -1,16 +1,12 @@
 //ajax远程调用
 import axios from '~Config/axios/httpConfig'
 import {HttpUtil} from '~Config/axios/httpUtil'
-//包装param参数
-import qs from 'qs'
-
-/* 不要使用 // 进行注释！！！！！！！！！！！！！！！！！！！！！！！！   */
 
 export const AnnouncementMyCreateListApi = {
     getAllAnnouncementTagEnums() {  //取得 公告标签 列表
         let obj = HttpUtil.formatQueryPage({},{});
         //查询所有公告标签信息
-        return axios.post('/oblCtl/announcementTag/gainEnumSelect',qs.stringify(obj)).then(res => res.data);
+        return axios.post('/oblCtl/announcementTag/gainEnumSelect',obj).then(res => res.data);
     },
     getPageQueryOfMyCreate(queryArr,tableConf) {   //取得 所有公告
         let obj = HttpUtil.formatQueryPage(queryArr,tableConf);
