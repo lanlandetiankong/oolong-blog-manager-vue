61f2d709f7f4a949130d9d0051e893b99ab10cc3
lanlandetiankong <695605813@qq.com>
2021-02-07 10:10:59 +0800

    接口url路径有误

--- src/pages/index/obl/article/collectCategory/OblArticleCollectCategoryApi.js
+++ src/pages/index/obl/article/collectCategory/OblArticleCollectCategoryApi.js
@@ -11,34 +11,34 @@ export const OblArticleCollectCategoryApi = {
         var obj = {
             withRoot:true  ,   //是否包含根节点
         }
-        return axios.post('/oblCtl/oblArticleCategory/queryTreeSelect',qs.stringify(obj)).then(res => res.data);
+        return axios.post('/oblCtl/oblArticleCollectCategory/queryTreeSelect',qs.stringify(obj)).then(res => res.data);
     },
     getPageQuery(queryArr,tableConf) {
         let obj = HttpUtil.formatQueryPage(queryArr,tableConf);
-        return axios.post('/oblCtl/oblArticleCategory/queryPage',obj).then(res => res.data);
+        return axios.post('/oblCtl/oblArticleCollectCategory/queryPage',obj).then(res => res.data);
     },
     getItemById(fid){  //根据id查询item
         var params = {
             fid:fid
         }
-        return axios.post("/oblCtl/oblArticleCategory/queryOneById",qs.stringify(params)).then(res => res.data) ;
+        return axios.post("/oblCtl/oblArticleCollectCategory/queryOneById",qs.stringify(params)).then(res => res.data) ;
     },
     createByForm(formObj) {     //新增
-        return axios.post("/oblCtl/oblArticleCategory/createByForm",qs.stringify(formObj)).then(res => res.data);;
+        return axios.post("/oblCtl/oblArticleCollectCategory/createByForm",qs.stringify(formObj)).then(res => res.data);;
     },
     updateByForm(formObj) {  //更新
-        return axios.post("/oblCtl/oblArticleCategory/updateByForm",qs.stringify(formObj)).then(res => res.data) ;
+        return axios.post("/oblCtl/oblArticleCollectCategory/updateByForm",qs.stringify(formObj)).then(res => res.data) ;
     },
     batchDeleteByIds(ids) {  //批量删除
         var obj = {
             delIds:ids
         }
-        return axios.post("/oblCtl/oblArticleCategory/batchDeleteByIds",qs.stringify(obj,{indices: false})).then(res => res.data) ;
+        return axios.post("/oblCtl/oblArticleCollectCategory/batchDeleteByIds",qs.stringify(obj,{indices: false})).then(res => res.data) ;
     },
     deleteById(delId) {  //删除
         var obj = {
             delId:delId
-        }
-        return axios.post("/oblCtl/oblArticleCategory/deleteById",qs.stringify(obj,{indices: false})).then(res => res.data) ;
+        };
+        return axios.post("/oblCtl/oblArticleCollectCategory/deleteById",obj).then(res => res.data) ;
     }
 }
