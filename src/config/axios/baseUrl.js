c7a90fee4b734631efa795a8af1e255c5a21c74f
lanlandetiankong <695605813@qq.com>
2021-03-09 16:00:39 +0800

    #VersionUpdate v1.0.2-SNAPSHOT -> v1.0.3-SNAPSHOT

--- src/config/axios/baseUrl.js
+++ src/config/axios/baseUrl.js
@@ -1,3 +1,3 @@
-const contextPath = '1.0.2-SNAPSHOT' ;
+const contextPath = '1.0.3-SNAPSHOT' ;
 const baseUrl = process.env.NODE_ENV === 'production' ? 'http://api.oblmg.tharlo.space/'+contextPath : 'http://localhost:20100/'+contextPath ;
 export default baseUrl
