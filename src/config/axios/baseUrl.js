8b78cb9baa8fcaefaae97e77d6f3d5c1fc626530
lanlandetiankong <695605813@qq.com>
2021-07-01 13:56:16 +0800

    #Optimization 1、baseUrl ip变更

--- src/config/axios/baseUrl.js
+++ src/config/axios/baseUrl.js
@@ -1,3 +1,4 @@
 const contextPath = '1.0.4-SNAPSHOT' ;
-const baseUrl = process.env.NODE_ENV === 'production' ? 'http://oblmg.api.tharlo.space/'+contextPath : 'http://localhost:20100/'+contextPath ;
+//const baseUrl = process.env.NODE_ENV === 'production' ? 'http://oblmg.api.tharlo.space/'+contextPath : 'http://localhost:20100/'+contextPath ;
+const baseUrl = process.env.NODE_ENV === 'production' ? 'http://121.40.58.132:20100/'+contextPath : 'http://localhost:20100/'+contextPath ;
 export default baseUrl
